5eeb5331bf8fd1113d6e597451c891e06a6e5e6c
August Valera <4U6U57@gmail.com>
2016-12-14 00:02:33 -0800

    Started init

--- sneak.peeker.py
+++ sneak.peeker.py
@@ -2,21 +2,25 @@
 
 import sys
 import argparse
+import os
+
+# Global variables
+prog = os.path.basename(__file__)
+prog_dir = os.path.abspath(".snk")
 
 def main():
 
     # Top level parser
     parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(title='subcommands',
-            description='valid subcommands')
+    subparsers = parser.add_subparsers(title='subcommands', description='valid subcommands')
 
     # Parser for init
     parser_init = subparsers.add_parser("init", help="Initializes the current directory")
-    parser_init.add_argument("FILE", help="Test files to add and process")
     parser_init.set_defaults(func=init)
     
     # Parser for add
     parser_add = subparsers.add_parser("add", help="Adds test files and processes them")
+    parser_add.add_argument("tests", metavar="FILE", help="Test file to add and process", nargs='+')
     parser_add.set_defaults(func=add)
 
     # Parser for commit
@@ -27,9 +31,16 @@ def main():
     args = parser.parse_args()
     args.func(args)
 
+# Creates .snk dir in current directory
 def init(args):
     print "INIT"
+    if os.path.isdir(prog_dir):
+        print "Existing %s directory in %s" % (prog, prog_dir)
+        exit(1)
+    os.makedirs(prog_dir) 
+    print "Initialized empty %s directory in %s" % (prog, prog_dir)
 
+# Parses 
 def add(args):
     print "ADD"
 
